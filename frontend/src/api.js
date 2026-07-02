1c93981f908f8d109f07ef967e3aebf1e817b36e
Darshini <darshinishah2020@gmail.com>
2026-07-02 15:34:00 +0530

    for deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000';
+const API_BASE = window.location.hostname === 'localhost'
+  ? 'http://localhost:8000'
+  : 'https://match-point-d8rv.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
